30a6e38461c529577d4559f31ece1feafb37d613
Vera Antamanova <vantamanova@yandex.ru>
2025-01-09 12:25:06 +0400

    W01 Individual Activity: Dynamic Arrays and Performance Analysis

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -1,8 +1,10 @@
-public static class Divisors {
+public static class Divisors
+{
     /// <summary>
     /// Entry point for the Divisors class
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         List<int> list = FindDivisors(80);
         Console.WriteLine("<List>{" + string.Join(", ", list) + "}"); // <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
         List<int> list1 = FindDivisors(79);
@@ -16,9 +18,25 @@ public static class Divisors {
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
-    private static List<int> FindDivisors(int number) {
+    private static List<int> FindDivisors(int number)
+    {
         List<int> results = new();
         // TODO problem 1
+
+        for (int i = 1; i <= Math.Sqrt(number); i++) // Loop from 1 to sqrt(number)
+        {
+            if (number % i == 0) // check if i is a divisor
+            {
+                results.Add(i);
+                int pair = number / i; // calculate the paired divisor for i
+
+                if (pair != number) // checking if pair is not a number
+                {
+                    results.Add(pair);
+                }
+            }
+        }
+        results.Sort();
         return results;
     }
 }
