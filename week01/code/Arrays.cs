b899b1da200aa14d4710d766071bcea83727021b
Vera Antamanova <vantamanova@yandex.ru>
2025-01-09 17:39:46 +0400

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,17 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Create an array to store the results, with the specified length
+        var result = new double[length];
+
+        // Multiply the number by each integer from 1 to 'length' using a for loop
+        // (Iteration starts from 1 to include the last number)
+        for (int i = 1; i <= length; i++)
+        {
+            double newNum = number * i; // Calculate the multiple of the number
+            result[i - 1] = newNum; // Store it in the array. Index starts from 0
+        }
+        return result; // Return an array of multiples of a number
     }
 
     /// <summary>
@@ -29,5 +41,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // get last elements from the list
+        var lastEmel = data.GetRange(data.Count - amount, amount);
+
+        // get the remaining elements
+        var startingElem = data.GetRange(0, data.Count - amount);
+
+        // clear list and add slices back
+        data.Clear();
+        data.AddRange(lastEmel);
+        data.AddRange(startingElem);
     }
 }
